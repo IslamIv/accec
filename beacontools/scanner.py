d9042d69c6c98db67e8de54a4a69cda2ca76227c
root <islam.ashraf@ivsystmes-eg.com>
2020-02-06 00:58:30 -0600

    agadga

--- beacontools/scanner.py
+++ beacontools/scanner.py
@@ -163,10 +163,13 @@ class Monitor(threading.Thread):
 
     def process_packet(self, pkt):
         """Parse the packet and call callback if one of the filters matches."""
-        print("NEW!!!>>>", pkt)
         bt_addr = bt_addr_to_string(pkt[7:13])
-        print("ADDRESS>>>", bt_addr)
-        print("Length>>>",len(pkt))
+        if bt_addr == "d7:61:29:e3:42:db":
+            print("ADD>>>", bt_addr)
+            print("LEn>>>", len(pkt))
+            print("PKT>>>", pkt)
+        #print("ADDRESS>>>", bt_addr)
+        #print("Length>>>",len(pkt))
         # check if this could be a valid packet before parsing
         # this reduces the CPU load significantly
         if not ( \
@@ -179,9 +182,9 @@ class Monitor(threading.Thread):
         #print("bt_addr>>>", bt_addr)
         rssi = bin_to_int(pkt[-1])
         # strip bluetooth address and parse packet
-        print("Before Parsing>>>", pkt)
+        #print("Before Parsing>>>", pkt)
         packet = parse_packet(pkt[14:-1])
-        print("After Parsing>>>",packet)
+        #print("After Parsing>>>",packet)
         # return if packet was not an beacon advertisement
         if not packet:
             return
