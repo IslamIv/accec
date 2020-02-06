3d419643893e0da67af7cacf69c8cdbfd75ca727
root <islam.ashraf@ivsystmes-eg.com>
2020-02-06 04:41:11 -0600

    agajea

--- beacontools/access.py
+++ beacontools/access.py
@@ -7,7 +7,7 @@ def acceess(packet):
     #ex = "0201060303aafe1316aafe807720003c00c4ffdcfff0fb0cfc0300"
     #print(len(ex))
     packet = array.array('B', packet)
-    packet.reverse()
+    #packet.reverse()
     hexi = hexlify(packet.tostring())
     print("hexi>>> ", hexi)
     print(len(hexi))
